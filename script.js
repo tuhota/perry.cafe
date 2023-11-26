a9d79fc7d72e96ae5896cd833fb524c47f95da5d
tuhota <96323290+tuhota@users.noreply.github.com>
2023-11-27 02:27:23 +1100

    Update script.js

--- script.js
+++ script.js
@@ -57,41 +57,10 @@ darkMode = true;
 
 windowWidth = window.innerWidth
 
-//document.getElementById("dark-mode").innerHTML="DARK 🌚"
-
 document.getElementById("view-body").style.width=windowWidth/2 + "px";
 
-//document.getElementById("nav").style.(marginLeft=windowWidth/4) + "px"
 document.getElementById("nav").style.marginLeft=(windowWidth/4) - 170 + "px"
 
 currentPage = "resume-nav";
 document.getElementById(currentPage).style.border="1px solid #d6d6d6"
 document.getElementById(currentPage).style.backgroundColor="rgb(16, 16, 16)"
-
-//showResume()
-
-//window.alert(bodyWidth + "px");
-//console.log(5 + 6);
-/*
-let s = "aaa"
-
-function myFunction() {
-
-// Display data from the object:
-//document.getElementById("demo").innerHTML = person.fullName();
-}
-
-const person = {
-  firstName: "John",
-  lastName: "Doe",
-  id: 5566,
-  fullName: function() {
-    return this.firstName + " " + this.lastName;
-  }
-};
-
-for (let i = 0; i < 5; i++) {
-  text += "The number is " + i + "<br>";
-}
-*/
-
